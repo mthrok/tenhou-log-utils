0856de89c7d7d05218cab2f0175e45b6b744302d
moto <mthrok@gmail.com>
2017-06-10 21:55:18 -0700

    Make AnKan/MinKan explicity

--- tenhou_log_utils/analyzer/analyzer.py
+++ tenhou_log_utils/analyzer/analyzer.py
@@ -8,120 +8,73 @@ from .components import Game
 _LG = logging.getLogger(__name__)
 
 
-def _validate_scores(prev_players, current_players):
-    for i, (prev_p, cur_p) in enumerate(zip(prev_players, current_players)):
-        if prev_p.score != cur_p.score:
-            raise AssertionError(
-                'Score from the previous round and the new round is different.'
-                ' Player %s; %s (expected: %s)' % (i, prev_p.score, cur_p.score)
-            )
-
-
-
 def _process_go(game, data):
-    _LG.info('Configuring game.')
     game.set_table(data['table'])
     game.set_config(data['config'])
-
-
-def _process_un(game, data):
-    game.init_players(data)
-
-
-def _process_init(game, data):
-    game.init_round(data)
-    if game.past_rounds:
-        _validate_scores(game.past_rounds[-1].players, game.round.players)
-
-def _process_draw(game, data):
-    game.round.draw(**data)
-
-
-def _process_discard(game, data):
-    game.round.discard(**data)
-
-
-def _process_call(game, data):
-    game.round.call(**data)
-
-
-def _process_dora(game, data):
-    game.round.state.dora.append(data['hai'])
-
-def _process_reach(game, data):
-    game.round.reach(**data)
 
 
 def _process_agari(game, data):
     game.round.agari(**data)
     game.archive_round()
     if 'result' in data:
-        uma = [res[1] for res in data['result']]
-        game.set_uma(uma)
-
+        game.set_uma(data['result']['uma'])
 
 def _process_ryuukyoku(game, data):
     game.round.ryuukyoku(**data)
     game.archive_round()
     if 'result' in data:
-        uma = [res[1] for res in data['result']]
-        game.set_uma(uma)
+        game.set_uma(data['result']['uma'])
 
 
-def _process_bye(game, data):
-    game.round.bye(**data)
-
-
-def _process_resume(game, data):
-    game.round.resume(**data)
+def _process_node(game, tag, data):
+    _LG.debug('%s: %s', tag, data)
+    if tag == 'INIT':
+        game.init_round(data)
+    elif tag == 'DRAW':
+        game.round.draw(**data)
+    elif tag == 'DISCARD':
+        game.round.discard(**data)
+    elif tag == 'CALL':
+        game.round.call(**data)
+    elif tag == 'DORA':
+        game.round.state.dora.append(data['hai'])
+    elif tag == 'REACH':
+        game.round.reach(**data)
+    elif tag == 'AGARI':
+        _process_agari(game, data)
+    elif tag == 'RYUUKYOKU':
+        _process_ryuukyoku(game, data)
+    elif tag == 'BYE':
+        game.round.bye(**data)
+    elif tag == 'RESUME':
+        game.round.resume(**data)
+    else:
+        raise NotImplementedError(u'%s: %s' % (tag, data))
+    if tag in ['INIT', 'CALL', 'REACH', 'AGARI', 'RYUUKYOKU']:
+        _LG.info('%s: %s', tag, data)
+        _LG.info(game)
 
 
 def _analyze_mjlog(game, parsed_log_data):
     # Skip SHUFFLE and TAIKYOKU tag
     _process_go(game, parsed_log_data['meta']['GO'])
-    _process_un(game, parsed_log_data['meta']['UN'])
-
+    game.init_players(parsed_log_data['meta']['UN'])
     for round_ in parsed_log_data['rounds']:
-        for result in round_:
-            tag, data = result['tag'], result['data']
-            _LG.debug('%s: %s', tag, data)
-            if tag == 'INIT':
-                _process_init(game, data)
-            elif tag == 'DRAW':
-                _process_draw(game, data)
-            elif tag == 'DISCARD':
-                _process_discard(game, data)
-            elif tag == 'CALL':
-                _process_call(game, data)
-            elif tag == 'DORA':
-                _process_dora(game, data)
-            elif tag == 'REACH':
-                _process_reach(game, data)
-            elif tag == 'AGARI':
-                _process_agari(game, data)
-            elif tag == 'RYUUKYOKU':
-                _process_ryuukyoku(game, data)
-            elif tag == 'BYE':
-                _process_bye(game, data)
-            elif tag == 'RESUME':
-                _process_resume(game, data)
-            else:
-                raise NotImplementedError('%s: %s' % (tag, data))
-            if tag in ['INIT', 'CALL', 'REACH', 'AGARI', 'RYUUKYOKU']:
-                _LG.info('%s: %s', tag, data)
-                _LG.info(game)
+        for event in round_:
+            _process_node(game, event['tag'], event['data'])
+
+
+def _try_print(game):
+    try:
+        _LG.info('\n%s', game)
+    except Exception:
+        pass
 
 
 def analyze_mjlog(parsed_log_data):
     game = Game()
-    failed = False
-    _error = Exception
     try:
         _analyze_mjlog(game, parsed_log_data)
     except Exception as error:
-        failed = True
-        _error = error
-
-    if failed:
-        _LG.error('\n%s', game.round)
-        raise _error
+        _try_print(game)
+        raise error
--- tenhou_log_utils/analyzer/components.py
+++ tenhou_log_utils/analyzer/components.py
@@ -101,15 +101,16 @@ class State(_ReprMixin, object):
 def _validate_tile_ranges(vals):
     for val in vals:
         if not 0 <= val <= 135:
-            raise ValueError('Invalid tile value (%s) was given' % val)
+            raise ValueError(u'Invalid tile value (%s) was given' % val)
 
 
 class Tiles(_ReprMixin, object):
     """Represent a set of tiles"""
-    def __init__(self, tiles, sort):
+    def __init__(self, tiles, sort, type=None):
         _validate_tile_ranges(tiles)
         self.tiles = tiles
         self.sort = sort
+        self.type = type
         if self.sort:
             self.tiles.sort()
 
@@ -131,7 +132,7 @@ class Tiles(_ReprMixin, object):
                 self.tiles.remove(tile)
             except ValueError:
                 raise ValueError(
-                    'Cannot remove %s from %s' % (tile, self.tiles)
+                    'Tile %s does not exist in %s' % (tile, self.tiles)
                 )
 
     def __len__(self):
@@ -146,11 +147,25 @@ class Tiles(_ReprMixin, object):
     def __iter__(self):
         return iter(self.tiles)
 
+    def __add__(self, items):
+        if isinstance(items, list):
+            return Tiles(self.tiles + items, self.sort, self.type)
+        if isinstance(items, int):
+            return Tiles(self.tiles + [items], self.sort, self.type)
+        raise NotImplementedError('Unexpected type %s' % type(items))
+
     def __eq__(self, other):
         if isinstance(other, list):
             return sorted(self.tiles) == sorted(other)
         if isinstance(other, Tiles):
             return sorted(self.tiles) == sorted(other.tiles)
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, list):
+            return sorted(self.tiles) != sorted(other)
+        if isinstance(other, Tiles):
+            return sorted(self.tiles) != sorted(other.tiles)
         return NotImplemented
 
 
@@ -200,21 +215,21 @@ class Hand(_ReprMixin, object):
             - 4 for 'MinKan'
         """
         if call_type in ['Pon', 'Chi', 'MinKan']:
-            self._expose_pon_kan_chi(mentsu)
+            self._expose_pon_kan_chi(mentsu, type=call_type)
         elif call_type == 'Nuki':
             self._expose_nuki(mentsu)
         elif call_type == 'AnKan':
-            self._expose_ankan(mentsu)
+            self._expose_ankan(mentsu, type=call_type)
         else:
-            raise NotImplementedError('%s: %s' % (call_type, mentsu))
+            raise NotImplementedError(u'%s: %s' % (call_type, mentsu))
 
     def _expose_nuki(self, mentsu):
         if len(mentsu) != 1:
-            raise AssertionError('Unexpected mentsu for Nuki.')
+            raise AssertionError(u'Unexpected mentsu for Nuki.')
         self.closed.remove(mentsu[0])
         self.nuki.add(mentsu[0])
 
-    def _expose_ankan(self, mentsu):
+    def _expose_ankan(self, mentsu, type):
         """Move tiles which compose An-Kan from covered to exposed
 
         Parameters
@@ -225,25 +240,26 @@ class Hand(_ReprMixin, object):
         base = mentsu[0] - mentsu[0] % 4
         full_mentsu = [base + i for i in range(4)]
         self.closed.remove(*full_mentsu)
-        self.exposed.append(Tiles(full_mentsu, sort=True))
+        self.exposed.append(Tiles(full_mentsu, sort=True, type=type))
 
-    def _expose_pon_kan_chi(self, mentsu):
+    def _expose_pon_kan_chi(self, mentsu, type):
         if sum([tile not in self.closed for tile in mentsu]) != 1:
             raise AssertionError(
                 'Exactly one tile of mentsu must be missing from hand.')
         for tile in mentsu:
             if tile in self.closed:
                 self.closed.remove(tile)
-        self.exposed.append(Tiles(mentsu, sort=True))
+        self.exposed.append(Tiles(mentsu, sort=True, type=type))
         self.menzen = False
 
-    def find_kan(self, base):
-        """Find the corresponding Kan mentsu from exposed mentsu"""
+    def find_pon(self, base):
+        """Find the corresponding Pon mentsu from exposed mentsu"""
         for mentsu in self.exposed:
-            for tile in mentsu:
-                if base == 4 * (tile // 4):
-                    return mentsu
-        raise ValueError('No corresponding Kan mentsu was found.')
+            if not mentsu.type == 'Pon':
+                continue
+            if base == 4 * (mentsu[0] // 4):
+                return mentsu
+        raise ValueError(u'No corresponding Pon mentsu was found.')
 
     def __contains__(self, tile):
         return any(tile in tiles for tiles in [self.closed] + self.exposed)
@@ -330,7 +346,7 @@ class Player(_ReprMixin, object):
 def _validate_last_draw(player, last_draw):
     if player != last_draw:
         raise AssertionError(
-            'Player who declared reach (%s) does not match '
+            u'Player who declared reach (%s) does not match '
             'to the on who drew a tile last (%s).' % (player, last_draw)
         )
 
@@ -338,22 +354,22 @@ def _validate_last_draw(player, last_draw):
 def _validate_discarded(mentsu, callee, last_discarded):
     if last_discarded['player'] != callee:
         raise AssertionError(
-            'Player who discarded last (%s) does not match callee (%s)'
+            u'Player who discarded last (%s) does not match callee (%s)'
             % (last_discarded['player'], callee)
         )
 
     if last_discarded['tile'] not in mentsu:
         raise AssertionError(
-            'Last discarded tile (%s) is not included in mentsu; %s'
+            u'Last discarded tile (%s) is not included in mentsu; %s'
             % (last_discarded, mentsu)
         )
 
 def _validate_score(players, scores):
-    for player, (score, _) in zip(players, scores):
+    for player, score in zip(players, scores):
         if not player.score == score:
             raise AssertionError(
                 (
-                    'Player score does not match with what is reported. '
+                    u'Player score does not match with what is reported. '
                     'Check implementation. Current: %s, Reported: %s'
                 ) % (player.score, score)
             )
@@ -363,14 +379,14 @@ def _validate_sticks(state, expected):
     if state.reach != expected['reach']:
         raise AssertionError(
             (
-                '#Reach sticks on table does not match with what is reported.'
+                u'#Reach sticks on table does not match with what is reported.'
                 'Check implementation. Current: %s, Reported: %s'
             ) % (state.reach, expected['reach'])
         )
     if state.combo != expected['combo']:
         raise AssertionError(
             (
-                '#Combo sticks on table does not match with what is reported.'
+                u'#Combo sticks on table does not match with what is reported.'
                 'Check implementation. Current: %s, Reported: %s'
             ) % (state.combo, expected['combo'])
         )
@@ -378,13 +394,22 @@ def _validate_sticks(state, expected):
 def _validate_furikomi(player, tile, last_discard):
     if player != last_discard['player']:
         raise AssertionError(
-            'Loser and the player who dicarded a tile '
+            u'Loser and the player who dicarded a tile '
             'does not match.'
         )
     if tile != last_discard['tile']:
         raise AssertionError(
-            'Machi tile and the tile lastly discarded '
+            u'Machi tile and the tile lastly discarded '
             'does not match.'
+        )
+
+
+def _validate_winning_hand(winner_hand, data):
+    hand = winner_hand.closed + data['machi'] if 'loser' in data else winner_hand.closed
+    if hand != data['hand']:
+        raise AssertionError(
+            'Winning hand is not contained the reported tile. '
+            'Check implementation.'
         )
 
 
@@ -393,12 +418,13 @@ def _validate_ryuukyoku_hands(players, hands):
         if hand is None:
             continue
         if player.hand.closed != hand:
-            raise ValueError(
-                (
-                    'Player hand does not match with what is reported. '
-                    'Check implementation. Current: %s, Reported: %s'
-                ) % (player.hand, hand)
-            )
+            expected = convert_hand(hand)
+            found = convert_hand(player.hand.closed.tiles)
+            message = (
+                u'Player hand does not match with what is reported. '
+                u'Check implementation. Expected: %s, Found: %s'
+            ) % (expected, found)
+            raise ValueError(ensure_str(message))
 
 
 class Round(_ReprMixin, object):
@@ -418,40 +444,77 @@ class Round(_ReprMixin, object):
         vals.extend(self.mode.to_repr(level=2))
         vals.append(u'  State:')
         vals.extend(self.state.to_repr(level=2))
+        if self.last_draw:
+            tile = convert_hand([self.last_draw['tile']])
+            vals.append(u'  Last Draw:')
+            vals.append(u'    Player: %s' % self.last_draw['player'])
+            vals.append(u'    tile: %s' %  tile)
         if self.last_discard:
             tile = convert_hand([self.last_discard['tile']])
-            vals.append('  Last Discard:')
-            vals.append('    Player: %s' % self.last_discard['player'])
-            vals.append('    tile: %s' %  tile)
+            vals.append(u'  Last Discard:')
+            vals.append(u'    Player: %s' % self.last_discard['player'])
+            vals.append(u'    tile: %s' %  tile)
         if self.players:
             vals.append(u'  Players:')
             for player in self.players:
                 vals.extend(player.to_repr(level=2))
         if self.ryuukyoku_reason:
-            vals.append('  Ryuukyoku: %s', self.ryuukyoku_reason)
+            vals.append(u'  Ryuukyoku: %s' % self.ryuukyoku_reason)
         return _indent(vals, level=level)
 
-    def get_top_players(self):
+    def init_players(self, tiles, scores):
+        """Initialize players
+
+        Parameters
+        ----------
+        tiles : list of ints
+        scores : list of ints
+        """
+        for index, (tiles_, score) in enumerate(zip(tiles, scores)):
+            if self.mode.sanma and index == 3:
+                break
+            hand = Hand(tiles_)
+            self.players.append(Player(index, hand, score))
+
+    def _get_top_players(self):
+        """Get the list of players with the highest scores"""
         players = sorted(self.players, key=lambda x: x.score, reverse=True)
         top_score = players[0].score
         return [player for player in players if player.score == top_score]
 
-    def init_players(self, tiles, scores):
-        for index, (tiles_, score) in enumerate(zip(tiles, scores)):
-            if self.mode.sanma and index == 3:
-                continue
-            hand = Hand(tiles_)
-            self.players.append(Player(index, hand, score))
-
     def draw(self, player, tile):
+        """Add new tile to player
+
+        Parameters
+        ----------
+        player : int
+        tile : int
+        """
         self.players[player].draw(tile)
         self.last_draw = {'player': player, 'tile': tile}
 
     def discard(self, player, tile):
+        """Remove a tile from player
+
+        Parameters
+        ----------
+        player : int
+        tile : int
+        """
         self.players[player].discard(tile)
         self.last_discard = {'player': player, 'tile': tile}
 
     def call(self, caller, callee, call_type, mentsu):
+        """Process call
+
+        Parameters
+        ----------
+        caller : int
+        callee : int
+        call_type : str
+            Either 'Chi', 'Pon', 'Nuki', 'AnKan', 'MinKan' or 'KaKan'
+        mentsu : list of ints
+        """
         caller_, callee_ = self.players[caller], self.players[callee]
         if call_type in ['Chi', 'Pon']:
             _validate_discarded(mentsu, callee, self.last_discard)
@@ -459,24 +522,42 @@ class Round(_ReprMixin, object):
             caller_.expose(call_type, mentsu)
         elif call_type == 'Nuki':
             caller_.expose(call_type, mentsu)
-        elif call_type == 'Kan':
-            if caller == callee:
-                caller_.expose('AnKan', mentsu)
-            else:
-                _validate_discarded(mentsu, callee, self.last_discard)
-                caller_.expose('MinKan', mentsu)
+        elif call_type == 'AnKan':
+            caller_.expose(u'AnKan', mentsu)
+        elif call_type == 'MinKan':
+            _validate_discarded(mentsu, callee, self.last_discard)
+            caller_.expose(u'MinKan', mentsu)
         elif call_type == 'KaKan':
-            base = 4 * (mentsu[0] // 4)
-            mentsu_ = caller_.hand.find_kan(base)
-            for tile in mentsu:
-                if tile not in mentsu_:
-                    mentsu_.add(tile)
-                    caller_.hand.closed.remove(tile)
+            self._update_kakan(caller_.hand, mentsu)
         else:
             raise NotImplementedError(
                 '%s: %s, %s, %s' % (call_type, caller, callee, convert_hand(mentsu)))
 
+    def _update_kakan(self, hand, mentsu):
+        """Update Pon to KaKan
+
+        Parameters
+        ----------
+        hand : Hand
+        mentsu : list of ints
+        """
+        base = 4 * (mentsu[0] // 4)
+        mentsu_ = hand.find_pon(base)
+        for tile in mentsu:
+            if tile not in mentsu_:
+                mentsu_.add(tile)
+                hand.closed.remove(tile)
+        mentsu_.type = 'KaKan'
+
     def reach(self, player, step, scores=None):
+        """Player declared reach
+
+        Parameters
+        ----------
+        player : int
+        step : int
+        scores : list of int or None
+        """
         _validate_last_draw(player, self.last_draw['player'])
         player_ = self.players[player]
         if step == 1:
@@ -485,68 +566,70 @@ class Round(_ReprMixin, object):
             player_.score -= 1000
             self.state.reach += 1
             if scores:
-                for player, score in zip(self.players, scores):
-                    if player.score != score:
-                        raise AssertionError(
-                            'Player score does not match to what is reported. '
-                            'Check implementation. Current: %s, Reported: %s'
-                        ) % (player.score, score)
+                _validate_score(self.players, scores)
         else:
-            raise NotImplementedError('Unexpected step value: {}'.format(step))
+            raise NotImplementedError(u'Unexpected step value: %s' % step)
 
-    def agari(self, scores, **data):
+    def agari(self, scores, gains, **data):
+        """
+        """
         _validate_score(self.players, scores)
         _validate_sticks(self.state, data['ba'])
+        _validate_winning_hand(self.players[data['winner']].hand, data)
         if 'loser' in data:
             _validate_furikomi(data['loser'], data['machi'][0], self.last_discard)
         else:
             _validate_last_draw(data['winner'], self.last_draw['player'])
 
-        for player, (_, gain) in zip(self.players, scores):
-            player.score += gain
-
-        self.state.reach = 0
-        # TODO: Combostick?
-
         # skip ten(Fu, Point, limit), yaku, yakuman, dora, ura-dora
-        _LG.info('Currently, no point conputation is carried out.')
+        _LG.info(u'Currently, no point conputation is carried out.')
 
-        # Validate winning hand
-        winner = self.players[data['winner']]
-        for tile in winner.hand.closed:
-            if tile not in data['hand']:
-                raise AssertionError(
-                    'Winning hand is not contained the reported tile. '
-                    'Check implementation.'
-                )
+        for player, gain in zip(self.players, gains):
+            player.score += gain
+        self.state.reach = 0
+
         if 'result' in data:
-            _validate_score(self.players, data['result'])
+            _validate_score(self.players, data['result']['scores'])
 
-    def ryuukyoku(self, hands, scores, ba, reason=None, result=None):
+    def _process_reach_sticks(self):
+        top_players = self._get_top_players()
+        if len(top_players) != 1:
+            raise NotImplementedError()
+        top_players[0].score += 1000 * self.state.reach
+        self.state.reach = 0
+
+    def ryuukyoku(self, hands, scores, gains, ba, reason=None, result=None):
         _validate_score(self.players, scores)
         _validate_sticks(self.state, ba)
         _validate_ryuukyoku_hands(self.players, hands)
 
-        for player, (_, diff) in zip(self.players, scores):
-            player.score += diff
+        for player, gain in zip(self.players, gains):
+            player.score += gain
 
         if reason:
             self.ryuukyoku_reason = reason
 
         if result:
             if self.state.reach:
-                top_players = self.get_top_players()
-                if len(top_players) != 1:
-                    raise NotImplementedError()
-                top_players[0].score += 1000 * self.state.reach
-                self.state.reach = 0
-            _validate_score(self.players, result)
+                self._process_reach_sticks()
+            _validate_score(self.players, result['scores'])
 
     def bye(self, index):
         self.players[index].available = False
 
     def resume(self, index, **_):
         self.players[index].available = True
+
+
+def _validate_round_scores(current_round, previous_round):
+    for prev_p, cur_p in zip(previous_round.players, current_round.players):
+        if prev_p.score != cur_p.score:
+            raise AssertionError(
+                (
+                    'The previous score does not match to new one. '
+                    'Check implementation. Current: %s, Reported: %s'
+                ) % (prev_p.score, cur_p.score)
+            )
 
 
 class Game(_ReprMixin, object):
@@ -585,16 +668,7 @@ class Game(_ReprMixin, object):
         self.round.init_players(hands, scores)
 
         if self.past_rounds:
-            cur_round = self.round
-            prev_round = self.past_rounds[-1]
-            for prev_p, cur_p in zip(prev_round.players, cur_round.players):
-                if prev_p.score != cur_p.score:
-                    raise AssertionError(
-                        (
-                            'The previous score does not match to new one. '
-                            'Check implementation. Current: %s, Reported: %s'
-                        ) % (prev_p.score, cur_p.score)
-                    )
+            _validate_round_scores(self.round, self.past_rounds[-1])
 
     def set_uma(self, uma):
         self.uma = uma
