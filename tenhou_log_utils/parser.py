082d252e80212d1c85f7d6c49967997b46f4db4c
Shintaro Minami <shintaro.minami@gmail.com>
2024-01-22 16:54:27 -0800

    Handle BYE and RESUME that happens before INIT (#3)

--- tenhou_log_utils/parser.py
+++ tenhou_log_utils/parser.py
@@ -466,6 +466,11 @@ def _structure_parsed_result(parsed):
             if round_ is not None:
                 game['rounds'].append(round_)
             round_ = [item]
+        elif tag in ['BYE', 'RESUME']:
+            if round_ is not None:
+                round_.append(item)
+            else:
+                game['meta'][tag] = data
         else:
             round_.append(item)
     game['rounds'].append(round_)
